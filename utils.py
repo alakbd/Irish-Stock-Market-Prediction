76da7ae390418db5024c6eb38593b6ea23056257
alakbd <74824186+alakbd@users.noreply.github.com>
2026-08-04 23:16:34 +0100

    Update utils.py
    y

--- utils.py
+++ utils.py
@@ -1,1 +1,20 @@
+import os
+import warnings
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import xgboost as xgb
+import yfinance as yf
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    roc_auc_score,
+    ConfusionMatrixDisplay
+)
+
+warnings.filterwarnings("ignore")
